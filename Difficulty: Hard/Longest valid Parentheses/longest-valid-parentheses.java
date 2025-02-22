05e29e3e96276b6942150dc929cb75cf2c881cc0
Rani Kushwaha <130595471+ranikushwah9754@users.noreply.github.com>
2025-02-22 12:13:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
@@ -1,39 +1,51 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
+        while (t-- > 0) {
             String S = in.readLine();
-            
+
             Solution ob = new Solution();
             System.out.println(ob.maxLength(S));
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
 
-class Solution{
-    static int maxLength(String S){
-        // code here
-        Stack<Integer>s=new Stack<>();
-        char[]arr=S.toCharArray();
-        int max=0;
-        for(int i=0;i<arr.length;i++){
-            if(!s.isEmpty() && arr[s.peek()]=='(' && arr[i]==')')s.pop();
-            else s.push(i);
-            int curr=(s.isEmpty())  ? i+1:i-s.peek();
-            max=Math.max(curr,max);
+class Solution {
+    static int maxLength(String s) {
+        int res = 0;
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(') {
+                st.push(i);
+            } else {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+                
+                if (!st.isEmpty()) {
+                    res = Math.max(res, i - st.peek());
+                } else {
+                    st.push(i);
+                }
+            }
         }
-        return max;
+        
+        return res;
     }
 }
