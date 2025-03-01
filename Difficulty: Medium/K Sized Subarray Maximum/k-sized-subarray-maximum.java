3345da8592a1a66422e8504a7ee343d972ff3409
Rani Kushwaha <130595471+ranikushwah9754@users.noreply.github.com>
2025-03-02 00:13:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -13,7 +13,6 @@ public class Main {
 
         while (t-- > 0) {
             // taking total number of elements
-            int k = Integer.parseInt(br.readLine());
             String line = br.readLine();
             String[] tokens = line.split(" ");
 
@@ -28,48 +27,37 @@ public class Main {
             int[] arr = new int[array.size()];
             int idx = 0;
             for (int i : array) arr[idx++] = i;
-            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
+            int k = Integer.parseInt(br.readLine());
+            ArrayList<Integer> res = new Solution().maxOfSubarrays(arr, k);
 
             // printing the elements of the ArrayList
             for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
             System.out.println();
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function template for JAVA
-
 class Solution {
-    // Function to find maximum of each subarray of size k.
-    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
-        // Your code here
-         ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        int tempMax= findMax(0,k-1,arr);
-        list.add(tempMax);
-        
-        for(int i=1; i<=arr.length-k; i++){
-            if(tempMax == arr[i-1] ){
-                tempMax = findMax(i, i+k-1,arr);
-                list.add(tempMax);
+    public ArrayList<Integer> maxOfSubarrays(int arr[], int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < arr.length; i++) {
+            // Remove indices of elements outside the current window
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
             }
-            else{
-                    tempMax = Math.max(tempMax, arr[i+k-1]);
-                    list.add(tempMax);
+            // Remove indices of elements that are smaller than the current element
+            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (i >= k - 1) {
+                result.add(arr[deque.peekFirst()]);
             }
         }
-        return list;
-        
-    }
-    
-    public int findMax(int start, int end, int arr[]){
-        int max=0;
-        while(start <= end){
-            max = Math.max(max, arr[start]);
-            start++;
-        }
-        return max;
+        return result;
     }
 }
